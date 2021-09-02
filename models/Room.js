345c1aa688065cd8514efb591fdbbb88a64c76c3
Joseph Timbol <joseph2000k@gmail.com>
2021-09-02 14:55:29 +0800

    added admin ref to room

--- models/Room.js
+++ models/Room.js
@@ -2,17 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RoomSchema = new Schema({
-    name: {
-        type: String
+  name: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  capacity: {
+    type: String,
+  },
+  admins: [
+    {
+      adminName: {
+        type: Schema.Types.ObjectId,
+        ref: "admins",
+      },
     },
-    Location: {
-        type: String
-    },
-    admins: [{
-        adminName: {
-            type: 
-        }
-    }]
-})
+  ],
+});
 
 module.exports = Room = mongoose.model("room", RoomSchema);
