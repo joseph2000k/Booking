58aea1c79231389859846da034727bc7d553a06d
Joseph Timbol <joseph2000k@gmail.com>
2021-09-13 14:06:28 +0800

    push approved meeting to rooms

--- routes/api/meeting.js
+++ routes/api/meeting.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
-const Meeting = require('../../models/Meeting');
-const OfficeProfile = require('../../models/OfficeProfile');
-const auth = require('../../middleware/auth');
-const Office = require('../../models/Office');
-const Room = require('../../models/Room');
-const authAdmin = require('../../middleware/authAdmin');
+const { check, validationResult } = require("express-validator");
+const Meeting = require("../../models/Meeting");
+const OfficeProfile = require("../../models/OfficeProfile");
+const auth = require("../../middleware/auth");
+const Office = require("../../models/Office");
+const Room = require("../../models/Room");
+const authAdmin = require("../../middleware/authAdmin");
 
 //@route    POST api/meeting/schedule
 //@desc     Create a meeting
 //@access   Private
 router.post(
-  '/schedule',
+  "/schedule",
   [
     auth,
-    check('room').custom((value) => {
+    check("room").custom((value) => {
       return Room.findById(value).then((room) => {
         if (!room) {
-          return Promise.reject('Invalid Room');
+          return Promise.reject("Invalid Room");
         }
       });
     }),
@@ -67,7 +67,7 @@ router.post(
       res.json(meeting);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
@@ -75,62 +75,94 @@ router.post(
 //@route    GET api/meeting
 //@desc     Get meeting for the current office
 //@access   Private
-router.get('/', auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const meeting = await Meeting.find({ office: req.office.id });
     res.json(meeting);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 //@route    DELETE api/meeting/:meeting_id
 //@desc     delete a meeting
 //@access   Private
-router.delete('/:meeting_id', auth, async (req, res) => {
+router.delete("/:meeting_id", auth, async (req, res) => {
   try {
     const meeting = await Meeting.findById(req.params.meeting_id);
 
     if (!meeting) {
-      return res.status(404).json({ msg: 'meeting not found' });
+      return res.status(404).json({ msg: "meeting not found" });
     }
 
     //check user
     if (meeting.office.toString() !== req.office.id) {
-      return res.status(401).json({ msg: 'user not authorized' });
+      return res.status(401).json({ msg: "user not authorized" });
     }
 
     if (meeting.finish) {
       return res
         .status(405)
-        .json({ msg: 'cannot be deleted, meeting is finished.' });
+        .json({ msg: "cannot be deleted, meeting is finished." });
     }
 
     await meeting.remove();
-    res.json({ msg: 'meeting removed' });
+    res.json({ msg: "meeting removed" });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
 //@route    PUT api/meeting/approval/:id
 //@desc     approve a meeting
 //@access   Private/admin
-router.put('/approval/:id', authAdmin, async (req, res) => {
+router.put("/approval/:id", authAdmin, async (req, res) => {
   try {
     const filter = { _id: req.params.id };
     const update = { isNotPending: true };
 
-    let meeting = await Meeting.findOneAndUpdate(filter, update, { new: true });
-    if (meeting) {
-      await meeting.save();
-      return res.json(meeting);
+    //get the room in the Meeting model-done
+    //IF room
+    //get rooms and store in an array
+    //for each rooms in array. get all rooms in the array then save meetings in each rooms
+
+    let updatedMeeting = await Meeting.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+
+    //look for all the rooms in meeting.rooms
+    const meetingRoom = updatedMeeting.rooms.map((item) => item.room);
+    let room = await Room.findById(meetingRoom);
+
+    if (updatedMeeting && room) {
+      room.meetings.unshift(updatedMeeting);
+      await updatedMeeting.save();
+      await room.save();
+      return res.json(updatedMeeting);
     }
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route    GET api/meeting/approval/
+//@desc     show all meetings that needs to be approve
+//@access   Private/admin
+router.get("/approval", authAdmin, async (req, res) => {
+  try {
+    const meeting = await Meeting.find();
+
+    const pendingMeeting = meeting.filter(
+      (item) => item.isNotPending === false
+    );
+
+    res.json(pendingMeeting);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
