2c01b398c74541e7b602a2fab392896faafb5209
Joseph Timbol <joseph2000k@gmail.com>
2021-10-20 20:41:29 +0800

    edit on landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,20 +1,17 @@
-import React from "react";
+import React from 'react';
 
 const Landing = () => {
   return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Developer Connector</h1>
-          <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
-            other developers
-          </p>
-          <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Rooms</h1>
+          <p className='lead'>Lists of room will be displayed in here.</p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
               Sign Up
             </a>
-            <a href="login.html" className="btn btn-light">
+            <a href='login.html' className='btn btn-light'>
               Login
             </a>
           </div>
