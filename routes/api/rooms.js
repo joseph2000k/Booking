301e812c51377bc76e332a33dc53f0dfa8245bf7
Joseph Timbol <joseph2000k@gmail.com>
2021-10-26 22:14:33 +0800

    find room by ID created

--- routes/api/rooms.js
+++ routes/api/rooms.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const authAdmin = require("../../middleware/authAdmin");
-const auth = require("../../middleware/auth");
-const Admin = require("../../models/Admin");
-const OfficeProfile = require("../../models/OfficeProfile");
-const Room = require("../../models/Room");
+const { check, validationResult } = require('express-validator');
+const authAdmin = require('../../middleware/authAdmin');
+const auth = require('../../middleware/auth');
+const Admin = require('../../models/Admin');
+const OfficeProfile = require('../../models/OfficeProfile');
+const Room = require('../../models/Room');
 
 //@route POST api/rooms
 //@desc Create a room
 //@access Private
 router.post(
-  "/",
-  [authAdmin, check("name", "room name is required").notEmpty()],
+  '/',
+  [authAdmin, check('name', 'room name is required').notEmpty()],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -25,14 +25,14 @@ router.post(
       if (req.admin.level !== 1) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "Invalid Credentials" }] });
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
       }
       let newRoom = await Room.findOne({ name });
 
       if (newRoom) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "room already exists" }] });
+          .json({ errors: [{ msg: 'room already exists' }] });
       }
 
       newRoom = new Room({
@@ -46,7 +46,7 @@ router.post(
       res.json(room);
     } catch (err) {
       console.err(err.message);
-      res.status(500).send("Server Error");
+      res.status(500).send('Server Error');
     }
   }
 );
@@ -54,30 +54,45 @@ router.post(
 //@route  GET api/rooms
 //@desc   Get all room
 //@access Public
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const rooms = await Room.find();
 
     res.json(rooms);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
 //@route  GET api/rooms/meetings
 //@desc   Get all approved meetings
 //@access Private
-router.get("/meetings", auth, async (req, res) => {
+router.get('/meetings', auth, async (req, res) => {
   try {
-    const rooms = await Room.find().populate("meetings", "rooms");
+    const rooms = await Room.find().populate('meetings', 'rooms');
     res.json(rooms);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.status(500).send('Server Error');
   }
 });
 
-//TODO: get next 5 meetings in all rooms
+//@route  GET api/rooms/:roomId
+//@desc   Get room by ID
+//@access Public
+
+router.get('/room/:roomId', async (req, res) => {
+  try {
+    const room = await Room.findById(req.params.roomId).populate(
+      'admins',
+      '-password'
+    );
+    res.json(room);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
