904b57255839304392da82f0ad3a043efb6923ca
Joseph Timbol <joseph2000k@gmail.com>
2022-01-28 12:16:08 +0800

    delete confirmation button on ForApproval Component

--- client/src/components/dashboard/ForApproval.js
+++ client/src/components/dashboard/ForApproval.js
@@ -1,10 +1,28 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Moment from "react-moment";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import ClockLoader from "react-spinners/ClockLoader";
+import Modal from "react-bootstrap/Modal";
 
 const ForApproval = ({ meetings, loading, deleteMeeting }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [deleteId, setDeleteId] = useState(null);
+
+  const handleDelete = () => {
+    deleteMeeting(deleteId);
+    handleClose();
+  };
+
+  const handleDeleteClick = (id) => {
+    setDeleteId(id);
+    handleShow();
+  };
+
   const allMeetings = meetings.map((meeting) => (
     <tr key={meeting._id}>
       <td>{meeting.description}</td>
@@ -15,7 +33,7 @@ const ForApproval = ({ meetings, loading, deleteMeeting }) => {
       <td>
         <button
           className="btn btn-danger"
-          onClick={() => deleteMeeting(meeting._id)}
+          onClick={() => handleDeleteClick(meeting._id)}
         >
           Delete
         </button>
@@ -35,6 +53,28 @@ const ForApproval = ({ meetings, loading, deleteMeeting }) => {
         </thead>
         <tbody>{loading ? <ClockLoader /> : allMeetings}</tbody>
       </table>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Meeting</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Are you sure you want to delete this meeting?</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-secondary" onClick={handleClose}>
+            Close
+          </button>
+          <button className="btn btn-danger" onClick={handleDelete}>
+            Delete
+          </button>
+        </Modal.Footer>
+      </Modal>
     </Fragment>
   );
 };
