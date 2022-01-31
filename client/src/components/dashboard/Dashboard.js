c7c28f5f33f3b984caec8748e0814f7e4cf80ed9
Joseph Timbol <joseph2000k@gmail.com>
2022-01-31 13:08:26 +0800

    for approval refactor

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,16 @@
-import React, { useEffect, Fragment } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { meetingHistory } from "../../actions/meeting";
-import { getMeetings } from "../../actions/meeting";
-import ClockLoader from "react-spinners/ClockLoader";
-import History from "./History";
-import ForApproval from "./ForApproval";
-import { clearSchedules } from "../../actions/meeting";
-import { getUpcomingMeetings } from "../../actions/meeting";
-import { deleteMeeting } from "../../actions/meeting";
-import UpcomingMeetings from "./UpcomingMeetings";
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { meetingHistory } from '../../actions/meeting';
+import { getMeetings } from '../../actions/meeting';
+import ClockLoader from 'react-spinners/ClockLoader';
+import History from './History';
+import ForApproval from './ForApproval';
+import { clearSchedules } from '../../actions/meeting';
+import { getUpcomingMeetings } from '../../actions/meeting';
+import { deleteMeeting } from '../../actions/meeting';
+import UpcomingMeetings from './UpcomingMeetings';
 
 const Dashboard = ({
   auth: { office, isSendingRequest },
@@ -29,25 +29,29 @@ const Dashboard = ({
     clearSchedules();
   }, [meetingHistory, getMeetings, getUpcomingMeetings, clearSchedules]);
 
+  const forApproval = meetings.filter(
+    (meeting) => meeting.isApproved === false
+  );
+
   return office === null || isSendingRequest ? (
-    <div className="d-flex justify-content-center">
+    <div className='d-flex justify-content-center'>
       <ClockLoader />
     </div>
   ) : (
     <Fragment>
       <div>
-        <h3 className="text-center">{office.officeName} Office Dashboard</h3>
-        <Link to="create-meeting">
-          <button className="btn btn-primary shadow m-3 position-fixed bottom-0 end-0">
+        <h3 className='text-center'>{office.officeName} Office Dashboard</h3>
+        <Link to='create-meeting'>
+          <button className='btn btn-primary shadow m-3 position-fixed bottom-0 end-0'>
             <h5>
-              <i class="fa fa-pencil-square" aria-hidden="true"></i> Schedule a
+              <i class='fa fa-pencil-square' aria-hidden='true'></i> Schedule a
               Meeting
             </h5>
           </button>
         </Link>
       </div>
       {meetings.length > 0 && (
-        <ForApproval meetings={meetings} deleteMeeting={deleteMeeting} />
+        <ForApproval meetings={forApproval} deleteMeeting={deleteMeeting} />
       )}
       {upcoming.length > 0 && <UpcomingMeetings upcoming={upcoming} />}
 
