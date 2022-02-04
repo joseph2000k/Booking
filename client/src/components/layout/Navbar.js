dbc78828829be57780ebe94784d82aab33057496
Joseph Timbol <joseph2000k@gmail.com>
2022-02-04 14:47:21 +0800

    navlink for rooms preparation

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -34,13 +34,40 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </h2>
 
-      {!loading && (
-        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
-      )}
+      <div className="d-flex align-items-center">
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle link-light"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Rooms
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </div>
+        </li>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+        )}
+      </div>
 
       {/* <div className='d-flex align-items-center'>
         <Link className='text-decoration-none link-light mx-4 ' to='/login'>
-          <i class='fa fa-sign-in' aria-hidden='true'></i> Login
+          <i className='fa fa-sign-in' aria-hidden='true'></i> Login
         </Link>
       </div> */}
     </nav>
