57b48ff55071111279261b4e7388be34157f0a17
Joseph Timbol <joseph2000k@gmail.com>
2022-05-27 15:59:16 +0800

    schedule verifier accepting the same scheduleId

--- middleware/scheduleVerifier.js
+++ middleware/scheduleVerifier.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 
 module.exports = async function (req, res, next) {
-  const { room, start, end } = req.body;
+  const { room, start, end, scheduleId } = req.body;
   let roomName = await Room.findById(room);
 
   if (!roomName) {
@@ -50,6 +50,7 @@ module.exports = async function (req, res, next) {
         start: "$schedules.start",
         end: "$schedules.end",
         isCancelled: "$schedules.isCancelled",
+        scheduleId: "$schedules._id",
         _id: 0,
       },
     },
@@ -62,13 +63,51 @@ module.exports = async function (req, res, next) {
     { $project: { room: 0 } },
   ]);
 
-  const schedule = meetingList.find(
-    (item) =>
-      (item.start.getTime() <= new Date(start).getTime() ||
-        item.start.getTime() <= new Date(end).getTime()) &&
-      (item.end.getTime() >= new Date(start).getTime() ||
-        item.end.getTime() >= new Date(end).getTime())
-  );
+  meetingArray = [];
+  for (let i = 0; i < meetingList.length; i++) {
+    if (
+      (moment(start).isBefore(meetingList[i].start) &&
+        moment(end).isAfter(meetingList[i].end)) ||
+      (moment(start).isSameOrAfter(meetingList[i].start) &&
+        moment(end).isSameOrBefore(meetingList[i].end))
+    ) {
+      meetingArray.push(meetingList[i]);
+    }
+    if (start === undefined) {
+      continue;
+    }
+  }
+
+  console.log(meetingArray);
+  if (meetingArray.length > 1) {
+    return res.json({ msg: "Please check date" });
+  } else if (meetingArray.length == 1 && meetingArray.start != undefined) {
+    console.log(meetingArray[0], "this is the meeting array");
+    if (meetingArray[0].scheduleId == scheduleId) {
+      return next();
+    } else if (meetingArray[0].scheduleId != scheduleId) {
+      return res.json({ msg: "Please check date" });
+    }
+  }
+
+  if (meetingArray.length == 0 || meetingArray[0].scheduleId != scheduleId) {
+    const schedule = meetingList.find(
+      (item) =>
+        (item.start.getTime() <= new Date(start).getTime() ||
+          item.start.getTime() <= new Date(end).getTime()) &&
+        (item.end.getTime() >= new Date(start).getTime() ||
+          item.end.getTime() >= new Date(end).getTime())
+    );
+    if (schedule) {
+      return res.status(406).json({
+        errors: [
+          {
+            msg: `Please check overlapping dates`,
+          },
+        ],
+      });
+    }
+  }
 
   const newSchedule = {
     room: roomName.id,
@@ -78,15 +117,7 @@ module.exports = async function (req, res, next) {
     end,
   };
 
-  if (schedule) {
-    return res.status(406).json({
-      errors: [
-        {
-          msg: `Please check overlapping dates`,
-        },
-      ],
-    });
-  } //if start is past or end is past return error
+  //if start is past or end is past return error
   if (moment(start).isBefore(moment()) || moment(end).isBefore(moment())) {
     return res.status(403).json({ errors: [{ msg: "invalid time" }] });
   } else {
