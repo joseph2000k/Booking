10be5d2ef3239cbc27834176bf114f6b5452e649
Joseph Timbol <joseph2000k@gmail.com>
2022-06-01 09:06:01 +0800

    Added tabs in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,19 @@
-import React, { useEffect, Fragment } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { getMeetings } from "../../actions/meeting";
-import ClockLoader from "react-spinners/ClockLoader";
-import History from "./History";
-import ForApproval from "./ForApproval";
-import ForApprovalofAdmin from "./admin/ForApprovalofAdmin";
-import { getForApprovalMeetings } from "../../actions/meeting";
-import { clearSubmitMeetings } from "../../actions/meeting";
-import { getSchedules } from "../../actions/meeting";
-import UpcomingMeetings from "./UpcomingMeetings";
-import moment from "moment";
+import React, { useEffect, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getMeetings } from '../../actions/meeting';
+import ClockLoader from 'react-spinners/ClockLoader';
+import History from './History';
+import ForApproval from './ForApproval';
+import ForApprovalofAdmin from './admin/ForApprovalofAdmin';
+import { getForApprovalMeetings } from '../../actions/meeting';
+import { clearSubmitMeetings } from '../../actions/meeting';
+import { getSchedules } from '../../actions/meeting';
+import UpcomingMeetings from './UpcomingMeetings';
+import moment from 'moment';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 const Dashboard = ({
   getForApprovalMeetings,
@@ -54,44 +56,60 @@ const Dashboard = ({
   }
 
   return office === null || isSendingRequest || loading ? (
-    <div className="d-flex justify-content-center">
+    <div className='d-flex justify-content-center'>
       <ClockLoader />
     </div>
   ) : (
     <Fragment>
       <div>
-        <h3 className="text-center">{office.officeName} Office Dashboard</h3>
-        <Link to="create-meeting">
-          <button className="btn btn-primary shadow m-3 position-fixed bottom-0 end-0">
+        <h3 className='text-center'>{office.officeName} Dashboard</h3>
+        <Link to='create-meeting'>
+          <button className='btn btn-primary shadow m-3 position-fixed bottom-0 end-0'>
             <h5>
-              <i class="fa fa-pencil-square" aria-hidden="true"></i> Schedule a
+              <i class='fa fa-pencil-square' aria-hidden='true'></i> Schedule a
               Meeting
             </h5>
           </button>
         </Link>
       </div>
 
-      {(office.role === "admin" || office.role === "manager") &&
-      adminApproval.length > 0 ? (
-        <ForApprovalofAdmin />
-      ) : null}
-      {forApproval.length > 0 && <ForApproval meetings={forApproval} />}
-      {upcomingMeetings.length > 0 && (
-        <UpcomingMeetings
-          upcoming={upcomingMeetings.sort((a, b) =>
-            moment(a.start).isAfter(moment(b.start)) ? 1 : -1
+      <Tabs
+        defaultActiveKey='upcomingMeetings'
+        id='uncontrolled-tab-example'
+        className='mb-3'
+      >
+        <Tab eventKey='upcomingMeetings' title='Upcoming Meetings'>
+          {upcomingMeetings.length > 0 && (
+            <UpcomingMeetings
+              upcoming={upcomingMeetings.sort((a, b) =>
+                moment(a.start).isAfter(moment(b.start)) ? 1 : -1
+              )}
+            />
           )}
-        />
-      )}
+        </Tab>
+        <Tab eventKey='forApprovalofAdmin' title='For Your Approval'>
+          {(office.role === 'admin' || office.role === 'manager') &&
+          adminApproval.length > 0 ? (
+            <ForApprovalofAdmin />
+          ) : null}
+        </Tab>
+        <Tab eventKey='forApproval' title='For Approval'>
+          {forApproval.length > 0 && <ForApproval meetings={forApproval} />}
+        </Tab>
 
-      {historyMeetings.length > 0 && (
-        <History
-          history={historyMeetings
-            .sort((a, b) => (moment(a.start).isAfter(moment(b.start)) ? -1 : 1))
-            .slice(0, 5)}
-          loading={loading}
-        />
-      )}
+        <Tab eventKey='history' title='History'>
+          {historyMeetings.length > 0 && (
+            <History
+              history={historyMeetings
+                .sort((a, b) =>
+                  moment(a.start).isAfter(moment(b.start)) ? -1 : 1
+                )
+                .slice(0, 5)}
+              loading={loading}
+            />
+          )}
+        </Tab>
+      </Tabs>
     </Fragment>
   );
 };
