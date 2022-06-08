d98f97ae7fb8ae5e7025719995242e618621dd70
Joseph Timbol <joseph2000k@gmail.com>
2022-06-08 14:42:08 +0800

    activeTab fixed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,19 +1,19 @@
-import React, { useEffect, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { getMeetings } from '../../actions/meeting';
-import ClockLoader from 'react-spinners/ClockLoader';
-import History from './History';
-import ForApproval from './ForApproval';
-import ForApprovalofAdmin from './admin/ForApprovalofAdmin';
-import { getForApprovalMeetings } from '../../actions/meeting';
-import { clearSubmitMeetings } from '../../actions/meeting';
-import { getSchedules } from '../../actions/meeting';
-import UpcomingMeetings from './UpcomingMeetings';
-import moment from 'moment';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
+import React, { useEffect, Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getMeetings } from "../../actions/meeting";
+import ClockLoader from "react-spinners/ClockLoader";
+import History from "./History";
+import ForApproval from "./ForApproval";
+import ForApprovalofAdmin from "./admin/ForApprovalofAdmin";
+import { getForApprovalMeetings } from "../../actions/meeting";
+import { clearSubmitMeetings } from "../../actions/meeting";
+import { getSchedules } from "../../actions/meeting";
+import UpcomingMeetings from "./UpcomingMeetings";
+import moment from "moment";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 
 const Dashboard = ({
   getForApprovalMeetings,
@@ -26,12 +26,15 @@ const Dashboard = ({
   adminApproval,
   deleteMeeting,
 }) => {
+  const [selectedTab, setSelectedTab] = useState(null);
   useEffect(() => {
     getForApprovalMeetings();
     getMeetings();
     clearSubmitMeetings();
     getSchedules();
   }, [getForApprovalMeetings, getMeetings, clearSubmitMeetings, getSchedules]);
+
+  const [activeTab, setActiveTab] = useState("upcomingMeetings");
 
   const forApproval = meetings.filter(
     (meeting) => meeting.isApproved === false && meeting.isNotPending === false
@@ -56,17 +59,17 @@ const Dashboard = ({
   }
 
   return office === null || isSendingRequest || loading ? (
-    <div className='d-flex justify-content-center'>
+    <div className="d-flex justify-content-center">
       <ClockLoader />
     </div>
   ) : (
     <Fragment>
       <div>
-        <h3 className='text-center'>{office.officeName} Dashboard</h3>
-        <Link to='create-meeting'>
-          <button className='btn btn-primary shadow m-3 position-fixed bottom-0 end-0'>
+        <h3 className="text-center">{office.officeName} Dashboard</h3>
+        <Link to="create-meeting">
+          <button className="btn btn-primary shadow m-3 position-fixed bottom-0 end-0">
             <h5>
-              <i class='fa fa-pencil-square' aria-hidden='true'></i> Schedule a
+              <i class="fa fa-pencil-square" aria-hidden="true"></i> Schedule a
               Meeting
             </h5>
           </button>
@@ -74,11 +77,19 @@ const Dashboard = ({
       </div>
 
       <Tabs
-        defaultActiveKey='upcomingMeetings'
-        id='uncontrolled-tab-example'
-        className='mb-3'
+        activeKey={
+          selectedTab === null && forApproval.length > 0
+            ? "forApproval"
+            : activeTab
+        }
+        onSelect={(key) => {
+          setSelectedTab(!null);
+          setActiveTab(key);
+        }}
+        id="uncontrolled-tab-example"
+        className="mb-3"
       >
-        <Tab eventKey='upcomingMeetings' title='Upcoming Meetings'>
+        <Tab eventKey="upcomingMeetings" title="Upcoming Meetings">
           {upcomingMeetings.length > 0 && (
             <UpcomingMeetings
               upcoming={upcomingMeetings.sort((a, b) =>
@@ -87,17 +98,17 @@ const Dashboard = ({
             />
           )}
         </Tab>
-        <Tab eventKey='forApprovalofAdmin' title='For Your Approval'>
-          {(office.role === 'admin' || office.role === 'manager') &&
+        <Tab eventKey="forApprovalofAdmin" title="For Your Approval">
+          {(office.role === "admin" || office.role === "manager") &&
           adminApproval.length > 0 ? (
             <ForApprovalofAdmin />
           ) : null}
         </Tab>
-        <Tab eventKey='forApproval' title='For Approval'>
+        <Tab eventKey="forApproval" title="For Approval">
           {forApproval.length > 0 && <ForApproval meetings={forApproval} />}
         </Tab>
 
-        <Tab eventKey='history' title='History'>
+        <Tab eventKey="history" title="History">
           {historyMeetings.length > 0 && (
             <History
               history={historyMeetings
