54442dfc01400184521c0f5f3d5928107fe7b558
Joseph Timbol <joseph2000k@gmail.com>
2022-06-16 14:02:15 +0800

    Redesign of Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,20 @@
-import React, { useEffect, Fragment, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { getMeetings } from '../../actions/meeting';
-import ClockLoader from 'react-spinners/ClockLoader';
-import History from './History';
-import ForApproval from './ForApproval';
-import ForApprovalofAdmin from './admin/ForApprovalofAdmin';
-import { getForApprovalMeetings } from '../../actions/meeting';
-import { clearSubmitMeetings } from '../../actions/meeting';
-import { getSchedules } from '../../actions/meeting';
-import UpcomingMeetings from './UpcomingMeetings';
-import moment from 'moment';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
-import Badge from 'react-bootstrap/Badge';
+import React, { useEffect, Fragment, useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { getMeetings } from "../../actions/meeting";
+import ClockLoader from "react-spinners/ClockLoader";
+import History from "./History";
+import ForApproval from "./ForApproval";
+import ForApprovalofAdmin from "./admin/ForApprovalofAdmin";
+import { getForApprovalMeetings } from "../../actions/meeting";
+import { clearSubmitMeetings } from "../../actions/meeting";
+import { getSchedules } from "../../actions/meeting";
+import UpcomingMeetings from "./UpcomingMeetings";
+import moment from "moment";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import Badge from "react-bootstrap/Badge";
 
 const Dashboard = ({
   getForApprovalMeetings,
@@ -35,7 +35,7 @@ const Dashboard = ({
     getSchedules();
   }, [getForApprovalMeetings, getMeetings, clearSubmitMeetings, getSchedules]);
 
-  const [activeTab, setActiveTab] = useState('upcomingMeetings');
+  const [activeTab, setActiveTab] = useState("upcomingMeetings");
 
   const forApproval = meetings.filter(
     (meeting) => meeting.isApproved === false && meeting.isNotPending === false
@@ -62,99 +62,101 @@ const Dashboard = ({
   const forApprovalBadge =
     forApproval.length > 0 ? (
       <Fragment>
-        For Approval{' '}
+        For Approval{" "}
         {forApproval.length > 0 && (
-          <Badge bg='primary'>{forApproval.length}</Badge>
+          <Badge bg="primary">{forApproval.length}</Badge>
         )}
       </Fragment>
     ) : (
-      'For Approval'
+      "For Approval"
     );
 
   const upcomingMeetingBadge =
     upcomingMeetings.length > 0 ? (
       <Fragment>
-        Upcoming Meetings{' '}
+        Upcoming Meetings{" "}
         {upcomingMeetings.length > 0 && (
-          <Badge bg='primary'>{upcomingMeetings.length}</Badge>
+          <Badge bg="primary">{upcomingMeetings.length}</Badge>
         )}
       </Fragment>
     ) : (
-      'Upcoming Meetings'
+      "Upcoming Meetings"
     );
 
   return office === null || isSendingRequest || loading ? (
-    <div className='d-flex justify-content-center'>
+    <div className="d-flex justify-content-center">
       <ClockLoader />
     </div>
   ) : (
-    <Fragment>
-      <div>
-        <h3 className='text-center'>{office.officeName} Dashboard</h3>
-        <Link to='create-meeting'>
-          <button className='btn btn-primary shadow m-3 position-fixed bottom-0 end-0'>
-            <h5>
-              <i class='fa fa-pencil-square' aria-hidden='true'></i> Schedule a
-              Meeting
-            </h5>
-          </button>
-        </Link>
-      </div>
+    <div className="container-light shadow-sm p-3 mb-5 bg-white rounded">
+      <Fragment>
+        <div>
+          <h3 className="text-center">{office.officeName} Dashboard</h3>
+          <Link to="create-meeting">
+            <button className="btn btn-primary shadow m-3 position-fixed bottom-0 end-0">
+              <h5>
+                <i class="fa fa-pencil-square" aria-hidden="true"></i> Schedule
+                a Meeting
+              </h5>
+            </button>
+          </Link>
+        </div>
 
-      <Tabs
-        {...(office.role === 'admin' && adminApproval.length > 0
-          ? {
-              activeKey:
-                selectedTab === null && adminApproval.length > 0
-                  ? 'forApprovalofAdmin'
-                  : activeTab,
-            }
-          : {
-              activeKey:
-                selectedTab === null && forApproval.length > 0
-                  ? 'forApproval'
-                  : activeTab,
-            })}
-        onSelect={(key) => {
-          setSelectedTab(!null);
-          setActiveTab(key);
-        }}
-        id='uncontrolled-tab-example'
-        className='mb-3'
-      >
-        <Tab eventKey='upcomingMeetings' title={upcomingMeetingBadge}>
-          {upcomingMeetings.length > 0 && (
-            <UpcomingMeetings
-              upcoming={upcomingMeetings.sort((a, b) =>
-                moment(a.start).isAfter(moment(b.start)) ? 1 : -1
-              )}
-            />
-          )}
-        </Tab>
-        <Tab eventKey='forApprovalofAdmin' title='For Your Approval'>
-          {(office.role === 'admin' || office.role === 'manager') &&
-          adminApproval.length > 0 ? (
-            <ForApprovalofAdmin />
-          ) : null}
-        </Tab>
-        <Tab eventKey='forApproval' title={forApprovalBadge}>
-          {forApproval.length > 0 && <ForApproval meetings={forApproval} />}
-        </Tab>
+        <Tabs
+          {...(office.role === "admin" && adminApproval.length > 0
+            ? {
+                activeKey:
+                  selectedTab === null && adminApproval.length > 0
+                    ? "forApprovalofAdmin"
+                    : activeTab,
+              }
+            : {
+                activeKey:
+                  selectedTab === null && forApproval.length > 0
+                    ? "forApproval"
+                    : activeTab,
+              })}
+          onSelect={(key) => {
+            setSelectedTab(!null);
+            setActiveTab(key);
+          }}
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="upcomingMeetings" title={upcomingMeetingBadge}>
+            {upcomingMeetings.length > 0 && (
+              <UpcomingMeetings
+                upcoming={upcomingMeetings.sort((a, b) =>
+                  moment(a.start).isAfter(moment(b.start)) ? 1 : -1
+                )}
+              />
+            )}
+          </Tab>
+          <Tab eventKey="forApprovalofAdmin" title="For Your Approval">
+            {(office.role === "admin" || office.role === "manager") &&
+            adminApproval.length > 0 ? (
+              <ForApprovalofAdmin />
+            ) : null}
+          </Tab>
+          <Tab eventKey="forApproval" title={forApprovalBadge}>
+            {forApproval.length > 0 && <ForApproval meetings={forApproval} />}
+          </Tab>
 
-        <Tab eventKey='history' title='History'>
-          {historyMeetings.length > 0 && (
-            <History
-              history={historyMeetings
-                .sort((a, b) =>
-                  moment(a.start).isAfter(moment(b.start)) ? -1 : 1
-                )
-                .slice(0, 5)}
-              loading={loading}
-            />
-          )}
-        </Tab>
-      </Tabs>
-    </Fragment>
+          <Tab eventKey="history" title="History">
+            {historyMeetings.length > 0 && (
+              <History
+                history={historyMeetings
+                  .sort((a, b) =>
+                    moment(a.start).isAfter(moment(b.start)) ? -1 : 1
+                  )
+                  .slice(0, 5)}
+                loading={loading}
+              />
+            )}
+          </Tab>
+        </Tabs>
+      </Fragment>
+    </div>
   );
 };
 
