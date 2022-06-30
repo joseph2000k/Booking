53c960f9d7757158d427168b86d8321234ff1d11
Joseph Timbol <joseph2000k@gmail.com>
2022-06-30 14:59:50 +0800

    Sidebar submenu for rooms created

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import { getRooms } from "../../actions/rooms";
 
-const Sidebar = ({}) => {
+const Sidebar = ({ getRooms, rooms, auth: { isAuthenticated } }) => {
+  useEffect(() => {
+    getRooms();
+  }, [getRooms]);
+
+  const roomLinks = rooms.map((room) => (
+    <MenuItem key={room._id}>
+      <Link to={`/rooms/${room._id}`}>{room.name}</Link>
+    </MenuItem>
+  ));
+
   return (
     <ProSidebar className="position-fixed top-0 start-0 mt-4">
       <div className="mt-4">
         <Menu>
-          <MenuItem icon={<i className="fa fa-home" />}>
-            <Link to="/">Home</Link>
-          </MenuItem>
-          <MenuItem icon={<i className="fa fa-door-open" />}>
-            <Link to="/rooms">Rooms</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/meetings">Meetings</Link>
-          </MenuItem>
-          <MenuItem>
-            <Link to="/dashboard">Dashboard</Link>
-          </MenuItem>
+          {isAuthenticated && (
+            <MenuItem icon={<i className="fa fa-home" />}>
+              <Link to="/dashboard">Dashboard</Link>
+            </MenuItem>
+          )}
+          <SubMenu
+            defaultOpen="true"
+            title="Rooms"
+            icon={<i className="fa fa-door-open" />}
+          >
+            {roomLinks}
+          </SubMenu>
         </Menu>
       </div>
     </ProSidebar>
   );
 };
 
-export default connect(null, {})(Sidebar);
+Sidebar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getRooms: PropTypes.func.isRequired,
+  rooms: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  rooms: state.room.rooms,
+});
+
+export default connect(mapStateToProps, { getRooms })(Sidebar);
